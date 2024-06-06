4372a3e9faffcb87a6eb6200d069c6c232bd338f
MaxlaserTechAlt <135626624+MaxlaserTechAlt@users.noreply.github.com>
2024-06-06 21:59:35 +0700

    Create updater.lua

---
+++ games/updater.lua
@@ -0,0 +1,51 @@
+local aliveCheck = function(player)
+	if player ~= lplr then 
+		if player.leaderstats.Bed.Value ~= '✅' and player.Character:WaitForChild('Humanoid').Health == 0 then return false elseif player.Character:WaitForChild('Humanoid').Health == 0 and player.leaderstats.Bed.Value == '✅' then return false end
+	end
+	if player.Character.Humanoid then
+		return player.Character.Humanoid.Health ~= 0
+	end
+    return false
+end
+local lplr = game.Players.LocalPlayer
+local getTarget = function(distance)
+	local magnitude, target = distance or math.huge, {}
+	if not aliveCheck(lplr) then 
+		return target 
+	end
+	for i,v in game.Players:GetPlayers() do 
+		local localpos = (aliveCheck(lplr) and lplr.Character.HumanoidRootPart.Position or Vector3.zero)
+		if v ~= lplr and aliveCheck(v) and aliveCheck(lplr) then
+			pcall(function()
+				local playerdistance = (localpos - v.Character.HumanoidRootPart.Position).Magnitude
+				if playerdistance < magnitude then 
+					magnitude = playerdistance
+					target.Human = true
+					target.RootPart = v.Character.HumanoidRootPart
+					target.Humanoid = v.Character:WaitForChild('Humanoid')
+					target.Player = v
+				end
+			end)
+		end
+	end
+	return target
+end
+local update = function()
+    local target = getTarget(25)
+    if target.RootPart then
+        shared.target = {
+            Humanoid = target.Humanoid,
+            Player = target.Player,
+            RootPart = target.RootPart,
+            Name = target.Player.Name
+        }
+    else
+        shared.target = {
+            Humanoid = nil,
+            Player = nil,
+            RootPart = nil,
+            Name = 'None'
+        }
+    end
+end
+shared.updateConnection = game:GetService('RunService').Stepped:Connect(update)
